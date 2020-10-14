2808f546a0e1ac7e7da06450814467c4fd3b954d
Tambe <AKUMARTA@volvocars.com>
2020-10-14 15:47:17 +0200

    Updated gitignore file

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,23 @@ package org.example;
  */
 public class App 
 {
+    int i;
+    static int j;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public void printMsg(){
+//        testStatic();
+        System.out.println(i);
+        System.out.println(j);
+    }
+
+    public static void testStatic(){
+//        printMsg();
+//        System.out.println(i);
+        System.out.println(j);
     }
 }
