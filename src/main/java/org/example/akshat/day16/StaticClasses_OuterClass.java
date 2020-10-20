eca243b32d21aebbb9c7c0cfac9a30cab42ee6c7
Tambe <AKUMARTA@volvocars.com>
2020-10-20 11:03:39 +0200

    Added static classes & fix for Gaurav's code on day 13

--- src/main/java/org/example/akshat/day16/StaticClasses_OuterClass.java
+++ src/main/java/org/example/akshat/day16/StaticClasses_OuterClass.java
@@ -1,10 +1,10 @@
-package org.example.akshat;
+package org.example.akshat.day16;
 
 /**
  * 1. Modifier 'static' can't be used here. Throws Complilation error.
  * 2. You cannot use the static keyword with a class unless it is an inner class.
  */
-public class Day14_StaticClasses {
+public class StaticClasses_OuterClass {
 
     public static void main(String[] args) {
         /**
@@ -15,25 +15,25 @@ public class Day14_StaticClasses {
          * exampleMethodFromInner_InnerClass()is a non-static method in a nested Inner static class.
          */
         //non-static method in a Inner static class.
-        new Day14_StaticClasses.Day14_Static_Inner().exampleMethodFromInnerClass();
-        new Day14_Static_Inner().exampleMethodFromInnerClass();
+        new StaticClasses_OuterClass.Day14_Static_Inner_01().exampleMethodFromInnerClass();
+        new Day14_Static_Inner_01().exampleMethodFromInnerClass();
 
         //non-static method in a nested Inner static class.
-        new Day14_Static_Inner.Day14_Static_InnerOfInner().exampleMethodFromInner_InnerClass();
+        new Day14_Static_Inner_01.Day14_Static_InnerOfInner().exampleMethodFromInner_InnerClass();
 
         /**
          * exampleStaticMethodFromInnerClass() is a Static method in a Inner static class.
          */
-        Day14_Static_Inner.exampleStaticMethodFromInnerClass();
-        Day14_StaticClasses.Day14_Static_Inner.exampleStaticMethodFromInnerClass();
+        Day14_Static_Inner_01.exampleStaticMethodFromInnerClass();
+        StaticClasses_OuterClass.Day14_Static_Inner_01.exampleStaticMethodFromInnerClass();
 
         /**
          * exampleStaticMethodFromInner_InnerClass() is a Static method in a nested Inner static class.
          */
-        Day14_Static_Inner.Day14_Static_InnerOfInner.exampleStaticMethodFromInner_InnerClass();
-        Day14_StaticClasses.Day14_Static_Inner.Day14_Static_InnerOfInner.exampleStaticMethodFromInner_InnerClass();
+        Day14_Static_Inner_01.Day14_Static_InnerOfInner.exampleStaticMethodFromInner_InnerClass();
+        StaticClasses_OuterClass.Day14_Static_Inner_01.Day14_Static_InnerOfInner.exampleStaticMethodFromInner_InnerClass();
 
-        //Compile time error. Parent of the nested static class Day14_Static_Inner class needs to be imported.
+        //Compile time error. Parent of the nested static class Day14_Static_Inner_01 class needs to be imported.
         //Day14_Static_InnerOfInner.exampleStaticMethodFromInner_InnerClass();
 
     }
@@ -42,7 +42,7 @@ public class Day14_StaticClasses {
      * Non-Static method of main class.
      */
     public void exampleMethodFromMainClass() {
-        System.out.println("This is a instance method of Outer class Day14_StaticClasses.!!");
+        System.out.println("This is a instance method of Outer class StaticClasses_OuterClass.!!");
     }
 
 
@@ -50,17 +50,17 @@ public class Day14_StaticClasses {
      * 1. Only Inner classes can be static and not the main Class.
      * 2. It can be accessed without instantiating the outer class.
      */
-    public static class Day14_Static_Inner {
+    public static class Day14_Static_Inner_01 {
 
         public void exampleMethodFromInnerClass() {
-            System.out.println("Day14_Static_Inner");
+            System.out.println("Day14_Static_Inner_01");
 
             //A method from main class can be called using the instance of the main class.
-            new Day14_StaticClasses().exampleMethodFromMainClass();
+            new StaticClasses_OuterClass().exampleMethodFromMainClass();
         }
 
         public static void exampleStaticMethodFromInnerClass() {
-            System.out.println("Static - Day14_Static_Inner");
+            System.out.println("Static - Day14_Static_Inner_01");
 
         }
 
@@ -73,7 +73,7 @@ public class Day14_StaticClasses {
                 System.out.println("Day14_Static_InnerOfInner");
 
                 //A method from main class can be called using the instance of the main class.
-                new Day14_StaticClasses().exampleMethodFromMainClass();
+                new StaticClasses_OuterClass().exampleMethodFromMainClass();
             }
 
             public static void exampleStaticMethodFromInner_InnerClass() {
