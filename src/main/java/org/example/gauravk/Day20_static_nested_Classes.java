72162be654180d5ca9160d22b024049dc1883325
Gaurav Khurana <gaurav.khurana@microsoft.com>
2020-10-28 18:21:07 +0530

    Added the pending program

---
+++ src/main/java/org/example/gauravk/Day20_static_nested_Classes.java
@@ -0,0 +1,4 @@
+package org.example.gauravk;
+
+public class Day20_static_nested_Classes {
+}
---
+++ src/main/java/org/example/gauravk/NumberOfObjects.java
@@ -0,0 +1,14 @@
+package org.example.gauravk;
+
+public class NumberOfObjects {
+
+    public static void main(String[] test)
+    {
+        String a="Udzial means Share";
+        String b="Udzial means Share";
+        String c="Udzial means Share";
+        String d="Udzial means Share";
+
+    }
+
+}
