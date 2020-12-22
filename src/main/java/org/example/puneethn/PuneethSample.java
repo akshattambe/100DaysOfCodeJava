8ffeff7b5d241027a8c9fe462fe15975f342e6cc
puneethnath86 <puneethnath86@users.noreply.github.com>
2020-12-22 23:12:47 +0530

    Pun 22 dec (#18)
    * Puneeth is commiting his Java file to pun_22Dec branch
    
    * Puneeth adding his code for the first time

--- src/main/java/org/example/puneethn/PuneethSample.java
+++ src/main/java/org/example/puneethn/PuneethSample.java
@@ -1,4 +1,8 @@
 package org.example.puneethn;
 
-public class PuneethSample {
+public class PuneethSample 
+{
+	public static void main(String[] args) {
+		System.out.println("Puneeth adding his code for the first time");
+	}
 }
