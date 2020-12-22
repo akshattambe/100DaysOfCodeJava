aca0c0bf281ce44c00702f638ea07ab295d88454
puneethnath86 <puneethnath86@users.noreply.github.com>
2020-12-22 21:04:54 +0530

    Puneeth is commiting his Java file to pun_22Dec branch

---
+++ src/main/java/org/example/puneeth/PunFirstClass.java
@@ -0,0 +1,10 @@
+package org.example.puneeth;
+
+public class PunFirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi this is puneeth and this is my first class");
+
+	}
+
+}
